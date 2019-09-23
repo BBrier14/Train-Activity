77996478ec0c597b28ceb3fe9a8fb6a74bb8b211
Benjamin Brier <ben.brier14@gmail.com>
2019-09-23 17:05:03 -0400

    connected firebase link, began working on the on-click event function

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,31 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDeOij3ke3Zgtb68cnDOlwSIm28hn169Ik",
+    authDomain: "train-90107.firebaseapp.com",
+    databaseURL: "https://train-90107.firebaseio.com",
+    projectId: "train-90107",
+    storageBucket: "",
+    messagingSenderId: "823295532375",
+    appId: "1:823295532375:web:9cc91a4247ebeb4bb67212"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+//This variable is a simple way to refer to the firebase database
+var database = firebase.database();
+
+//------------------ON CLICK EVENT------------------//
+//This On-click event will take the user input and push it into the firebase storage
+$("#add-train-button").on("click", function (event){
+    event.preventDefault();
+
+//On click part 2: this reads what the user inputs and turns it into a variable
+var trainName = $("#trainName-input").val().trim();  
+var destination = $("#destination-input").val().trim(); 
+var frequency = $("#frequency-input").val().trim();
+var minutes = $("#minutes-input").val().trim(); 
+
+//
+
+})
