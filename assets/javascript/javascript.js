b53edbc4eada57c71645da2406b072a922d9d95d
Benjamin Brier <ben.brier14@gmail.com>
2019-09-25 14:07:11 -0400

    formatted all Docs, removed console logs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,7 +17,7 @@ var database = firebase.database();
 
 //------------------ON CLICK EVENT------------------//
 //This On-click event will take the user input and push it into the firebase storage
-$("#add-train-btn").on("click", function(event) {
+$("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
     //On click part 2: this reads what the user inputs and turns it into a variable
@@ -37,11 +37,6 @@ $("#add-train-btn").on("click", function(event) {
     //this will push the trainCheck variable to firebase
     database.ref().push(trainCheck);
 
-    console.log(trainCheck.name);
-    console.log(trainCheck.destination);
-    console.log(trainCheck.frequency);
-    console.log(trainCheck.firstTrain);
-
     //this will clear the search bars of previous history
     $("#trainName-input").val('')
     $("#destination-input").val('')
@@ -55,71 +50,60 @@ $("#add-train-btn").on("click", function(event) {
 
 
 //This will take the data in firebase and attach it to the HTML
-database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
+database.ref().on("child_added", function (childSnapshot) {
+    
 
-//This stores the data from firebase into variables for later use
-var tName = childSnapshot.val().name;
-var tDestination = childSnapshot.val().destination;
-var tFrequency = childSnapshot.val().frequency;
-var tFirstTrain = childSnapshot.val().firstTrain;
-
-//This will console log the variables above
-console.log(tName);
-console.log(tDestination);
-console.log(tFrequency);
-console.log(tFirstTrain);
+    //This stores the data from firebase into variables for later use
+    var tName = childSnapshot.val().name;
+    var tDestination = childSnapshot.val().destination;
+    var tFrequency = childSnapshot.val().frequency;
+    var tFirstTrain = childSnapshot.val().firstTrain;
 
 //------------This is the list of variables and calculations to find frequency----------------//
 
-//this variable converts the first time found in firebase
-var firstTimeConverted = moment(tFirstTrain, "HH:mm").subtract(1, "years");
-console.log(firstTimeConverted);
+    //this variable converts the first time found in firebase
+    var firstTimeConverted = moment(tFirstTrain, "HH:mm").subtract(1, "years");
+    
 
-//this variable is the current time
-var currentTime = moment();
-console.log(currentTime);
+    //this variable is the current time
+    var currentTime = moment();
+    
 
-//this variable is the difference in times, in minutes
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log(diffTime);
+    //this variable is the difference in times, in minutes
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  
 
-//this variable finds the remainder of the diffTime variable and the specific train frequency
-var tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
+    //this variable finds the remainder of the diffTime variable and the specific train frequency
+    var tRemainder = diffTime % tFrequency;
+   
 
-//this variable shows the minutes to next train by subtracting the remainder from the frequency
-var tMinutesTillTrain = tFrequency - tRemainder;
-console.log(tMinutesTillTrain);
+    //this variable shows the minutes to next train by subtracting the remainder from the frequency
+    var tMinutesTillTrain = tFrequency - tRemainder;
+   
 
-//this variable finds when the next train is based on current time and the minutes until train variable
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log(nextTrain);
+    //this variable finds when the next train is based on current time and the minutes until train variable
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+   
 
 //-----------This completes the calculation section--------------//
 
+    //This creates a new row for each train added
+    var newRow = $("<tr>").append(
+        $("<td>").text(tName),
+        $("<td>").text(tDestination),
+        $("<td>").text(tFrequency),
+        $("<td>").text(moment(nextTrain).format("hh:mm a")),
+        $("<td>").text(tMinutesTillTrain)
 
+    )
 
-
-
-//This creates a new row for each train added
-var newRow = $("<tr>").append(
-    $("<td>").text(tName),
-    $("<td>").text(tDestination),
-    $("<td>").text(tFrequency),
-    $("<td>").text(moment(nextTrain).format("hh:mm a")),
-    $("<td>").text(tMinutesTillTrain)
-
-)
-
-$("#train-table > tbody").append(newRow);
+    $("#train-table > tbody").append(newRow);
 
 })
 
 
 // This will display the current time, as well as be the current time variable used to do the math
 var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
 var currentFormatted = "CURRENT TIME: " + moment(currentTime).format("hh:mm a");
-    $("#current-time").text(currentFormatted)
+$("#current-time").text(currentFormatted)
